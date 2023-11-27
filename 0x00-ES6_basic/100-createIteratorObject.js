0970a3c5a2d87c46503d7a08aaeac37dd66237a9
ahmadoa <ouladaouida@gmail.com>
2023-11-27 19:40:31 +0000

    updated syntax

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,6 +2,6 @@ export default function createIteratorObject(report) {
   const res = [];
   for (const department of Object.values(report.allEmployees)) {
     res.push(...department);
-  };
+  }
   return res;
 }
