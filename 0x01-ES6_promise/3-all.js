2dc9837d87412255314ce5ac3f848d90e0dc334c
ahmadoa <ouladaouida@gmail.com>
2023-11-29 22:02:37 +0000

    updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
-import { createUser, uploadPhoto } from "./utils";
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([createUser(), uploadPhoto()]).then((val) => {
+  return Promise.all([createUser(), uploadPhoto()]).then((values) => {
     console.log(
-      `${val[1].body} ${val[0].firstName} ${val[0].lastName}`,
+      `${values[1].body} ${values[0].firstName} ${values[0].lastName}`,
     );
   }).catch(() => {
     console.log('Signup system offline');
