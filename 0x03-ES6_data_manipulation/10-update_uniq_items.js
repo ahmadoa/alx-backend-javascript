999bbcaf9778ea1bdb2ceba1c7b1bf36aff7ef93
ahmadoa <ouladaouida@gmail.com>
2023-12-04 16:55:13 +0000

    removed a space :)

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -5,7 +5,7 @@ export default function updateUniqueItems(mapList) {
 
   for (const [key, val] of mapList) {
     if (val === 1) {
-       mapList.set(key, 100);
+      mapList.set(key, 100);
     }
   }
 
