53f761332f5ca5319a03e2ab8926baecd029d028
ahmadoa <ouladaouida@gmail.com>
2023-12-04 09:34:39 +0000

    function that returns an array of students for specific city with new grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(stdList, city, newGrades) {
+  if (!Array.isArray(stdList) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  const city_filtered = stdList.filter((std) => std.location === city);
+  const stdByCity = city_filtered.map((std) => {
+    const grade = newGrades.find((grade) => grade.studentId === std.id);
+    return { ...std, grade: grade ? grade.grade : 'N/A' };
+  });
+  return stdByCity;
+}
