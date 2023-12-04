40f4e61697ce3777c7bb25ae360dcecbe5268e32
ahmadoa <ouladaouida@gmail.com>
2023-12-04 16:48:51 +0000

    updated syntax errors

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(mapList) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   for (const [key, val] of mapList) {
     if (val === 1) {
        mapList.set(key, 100);
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const newlist = [];
   if (startString && typeof startString === 'string') {
     for (const value of set) {
-      if (value.startsWith(startString)) {
+      if (value && value.startsWith(startString)) {
         newlist.push(value.slice(startString.length));
       }
     }
