229984d2b8394fe2c6358eee76ad6407d9f2d84d
ahmadoa <ouladaouida@gmail.com>
2023-12-04 10:12:57 +0000

    function that returns a string of all set values that start with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  const newlist = [];
+  if (startString && typeof startString === 'string') {
+    for (const value of set) {
+      if (value.startsWith(startString)) {
+        newlist.push(value.slice(startString.length));
+      }
+    }
+  }
+  return newlist.join('-');
+}
