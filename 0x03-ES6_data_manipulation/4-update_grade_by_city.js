de4223342b2b898be008c9528f37341e170528d6
ahmadoa <ouladaouida@gmail.com>
2023-12-04 17:05:50 +0000

    updated

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(stdList, city, newGrades) {
   if (!Array.isArray(stdList) || !Array.isArray(newGrades)) {
     return [];
   }
-  const city_filtered = stdList.filter((std) => std.location === city).map((std) => {
+  const cityFiltered = stdList.filter((std) => std.location === city).map((std) => {
     const grade = newGrades.find((grade) => grade.studentId === std.id);
     return { ...std, grade: grade ? grade.grade : 'N/A' };
   });
 
-  return city_filtered;
+  return cityFiltered;
 }
