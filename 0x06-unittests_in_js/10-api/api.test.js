4087c4709147e30d390a5e5f1104d5cbf25b63c1
ahmadoa <ouladaouida@gmail.com>
2024-02-01 19:15:39 +0000

    added /available_payments & modified api test

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,40 +1,123 @@
-const express = require('express');
-const app = express();
-const port = 7865;
+const request = require('request');
+const expect = require('chai').expect;
 
-app.get('/', function (req, res) {
-  res.statusCode = 200;
-  res.send('Welcome to the payment system');
-});
-
-app.get('/cart/:id([0-9]+)', function (req, res) {
-  res.statusCode = 200;
-  res.send(`Payment methods for cart ${req.params.id}`);
-});
-
-app.get('/available_payments', (req, res) => {
-  res.set('Content-Type', 'application/json');
-  res.statusCode = 200;
-  res.send({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false,
-    },
+describe('Index page', function () {
+  const options = {
+    url: 'http://localhost:7865/',
+    method: 'GET',
+  };
+  it('check correct status code', function (done) {
+    request(options, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('check correct content', function (done) {
+    request(options, function (err, res, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
 
-app.post('/login', (req, res) => {
-  const userName = req.body.userName;
-  if (userName) {
-    res.statusCode = 200;
-    res.send(`Welcome ${userName}`);
-  } else {
-    res.status(404).send();
-  }
+describe('Cart page', function () {
+  it('check correct status code for correct url', function (done) {
+    request.get('http://localhost:7865/cart/12', function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('check correct content for correct url', function (done) {
+    request.get('http://localhost:7865/cart/12', function (err, res, body) {
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+  it('check correct status code for incorrect url', function (done) {
+    request.get('http://localhost:7865/cart/kim', function (err, res, body) {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
 });
 
-app.listen(port, function () {
-  console.log(`API available on localhost port ${port}`);
+describe('Available_payments page', function () {
+  it('check correct status for correct url', function () {
+    request.get(
+      'http://localhost:7865/available_payments',
+      (err, res, body) => {
+        if (err) {
+          expect(res.statusCode).to.not.equal(200);
+        } else {
+          expect(res.statusCode).to.equal(200);
+        }
+      }
+    );
+  });
+  it('check correct body content for correct url', function () {
+    const option = { json: true };
+    const payLoad = {
+      payment_methods: {
+        credit_cards: true,
+        paypal: false,
+      },
+    };
+    request.get(
+      'http://localhost:7865/available_payments',
+      option,
+      (err, res, body) => {
+        if (err) {
+          expect(res.statusCode).to.not.equal(200);
+        } else {
+          expect(body).to.deep.equal(payLoad);
+        }
+      }
+    );
+  });
 });
 
-module.exports = app;
+describe.skip('Login', function () {
+  it("check correct status code for request that's sent properly", function (done) {
+    const opt = {
+      url: 'http://localhost:7865/login',
+      json: true,
+      body: {
+        userName: 'JOE',
+      },
+    };
+    request.post(opt, function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it("check correct content for request that's sent properly", function (done) {
+    const opts = {
+      url: 'http://localhost:7865/login',
+      json: true,
+      body: {
+        userName: 'JOE',
+      },
+    };
+    request.post(opts, function (err, res, body) {
+      if (err) {
+        expect(res.statusCode).to.not.equal(200);
+      } else {
+        expect(body).to.contain('Welcome JOE');
+      }
+      done();
+    });
+  });
+  it("check correct status code for request that's not sent properly", function (done) {
+    const op = {
+      url: 'http://localhost:7865/login',
+      json: true,
+      body: {
+        usame: 'JOE',
+      },
+    };
+    request.post(op, function (err, res, body) {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
